e14bac6b77e4779ae8cfaa676ef60b14c9a4f3ba
Harish <harish.durairajan@ideas2it.com>
2023-03-08 15:11:00 +0530

    springApplication 6

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@GetMapping(value = "/spring")
 	public String welcome(){
-		return "hello Welcome";
+		return "hello Harish";
 	}
 
 	public static void main(String[] args) {
