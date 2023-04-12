d1b8cb1dd08465899fa3da5d8909c0e36f18c86d
palanisamy vignesh <vpalanisamy@ntst.com>
2023-04-12 10:47:07 +0530

    spring in EKS16

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@GetMapping(value = "/spring")
 	public String welcome(){
-		return "hello welcome";
+		return "Hello";
 	}
 
 	public static void main(String[] args) {
