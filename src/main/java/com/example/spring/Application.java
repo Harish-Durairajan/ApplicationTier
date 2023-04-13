a71ea6ceb33fc6f2ec8d34e1fcf79851d53fe55b
palanisamy vignesh <vpalanisamy@ntst.com>
2023-04-13 10:59:16 +0530

    spring in EKS22

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@GetMapping(value = "/spring")
 	public String welcome(){
-		return "spring boot";
+		return "springboot";
 	}
 
 	public static void main(String[] args) {
